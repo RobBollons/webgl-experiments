2f24711ece3dd1386464dd7fc1aaa6da062702fe
Robert Bollons <rob@robbollons.com>
2017-08-08 21:44:17 +0100

    attempted adding UVs;

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mandril = require('baboon-image');
 const resl = require('resl');
 const camera = require('regl-camera');
 const glsl = require('glslify');
+const parseObj = require('parse-wavefront-obj');
 
-const mainCamera = camera({
+const mainCamera = camera(regl, {
     center: [0,45,0],
     eye: [0,1,0],
     distance: 175
@@ -14,14 +15,23 @@ const manifest = {
     cube: {
         src: './assets/cube.obj',
         parser: parseObj
+    },
+    texture: {
+        src: './assets/nick-knowles.jpg',
+        type: 'image',
+        parser: (data) => regl.texture({
+            data,
+            mag: 'linear',
+            min: 'linear'
+        })
     }
-};
-
+}; 
 const drawCube = regl({
     frag: glsl.file('./index.js.frag.glsl'),
     vert: glsl.file('./index.js.vert.glsl'),
     attributes: {
-        position: regl.prop('position')
+        position: regl.prop('position'),
+        uv: regl.prop('uv')
     },
     elements: regl.prop('elements'),
     uniforms: {
@@ -30,14 +40,15 @@ const drawCube = regl({
 });
 
 resl({
-    manifest,
+    manifest: manifest,
     onDone: (data) => {
         regl.frame(({time}) => {
             mainCamera(() => {
                 drawCube({
-                    texture: mandril,
+                    texture: data.texture,
                     position: data.cube.positions,
-                    elements: data.cube.cells
+                    elements: data.cube.cells,
+                    uv: data.cube.vertexUVs
                 });
             });
         });
